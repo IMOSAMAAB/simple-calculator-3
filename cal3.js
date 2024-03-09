0351ff8a840f2db9857502786e5fc995379dea31
OSAMA <162601408+IMOSAMAAB@users.noreply.github.com>
2024-03-09 19:55:08 +0500

    1st commit
    it is my 1st commit

---
+++ cal3.js
@@ -0,0 +1,16 @@
+var result = document.getElementById("result");
+
+function display(number) {
+  result.value += number;
+}
+function calculator() {
+  var final_number = result.value;
+  var final_number = eval(final_number);
+  result.value = final_number;
+}
+function clrs() {
+  result.value = "";
+}
+function del() {
+  result.value = result.value.slice(0, -1);
+}
